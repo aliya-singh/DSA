e3204c7a04bddfb1c3aa2f6c84790a622cbce32e
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-14 19:27:09 +0530

    Time: 0 ms (100%), Space: 14.9 MB (56.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -4,18 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+
         n = len(nums)
-        m = n / 3
-        l = []
-        nums.sort()
-        count = 1
-        for i in range(1, n):
-            if nums[i-1] == nums[i]:
-                count += 1
+        m = n/3
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
             else:
-                if count > m:
-                    l.append(nums[i-1])
-                count = 1
-        if count > m:
-            l.append(nums[n-1])
+                d[i] += 1
+        
+        l = []
+        for i in d:
+            if d[i] > m:
+                l.append(i)
+        
         return l
