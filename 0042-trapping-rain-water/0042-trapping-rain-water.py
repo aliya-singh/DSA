cd386d60ad3c76e42c81a7d82add3dfb8a7cf3fd
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-19 04:01:35 +0530

    Time: 27 ms (30.22%), Space: 13.9 MB (31.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,23 +4,24 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
-        prefix = [height[0]]
         n = len(height)
+        l1 = [height[0]]
         for i in range(1, n):
-            x = max(prefix[i-1], height[i])
-            prefix.append(x)
-        
-        suffix = [0] * n
-        suffix[n-1] = height[n-1]
+            if l1[i-1] < height[i]:
+                l1.append(height[i])
+            else:
+                l1.append(l1[i-1])
+
+        l2 = [0] * n
+        l2[n-1] = height[n-1]
         for i in range(n-2, -1, -1):
-            x = max(height[i], suffix[i+1])
-            suffix[i] = x
+            if l2[i+1] > height[i]:
+                l2[i] = l2[i+1]
+            else:
+                l2[i] = height[i]
         
         count = 0
         for i in range(n):
-            if height[i] < suffix[i] and height[i] < prefix[i]:
-                count += min(suffix[i], prefix[i]) - height[i]
+            count += min(l1[i], l2[i]) - height[i]
         
         return count
-
