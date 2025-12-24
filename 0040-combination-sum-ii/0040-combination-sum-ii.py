49d36f60aa3fdbc7dad5196201145807b6b068c1
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-25 00:51:52 +0530

    Time: 47 ms (20.41%), Space: 12.6 MB (37.33%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def reccursion(self, candidates, target, ans, i):
+        if target == 0:
+            self.l.append(ans[:])
+            return
+        
+        if target < 0 or i == len(candidates):
+            return
+        
+        ans.append(candidates[i])
+        self.reccursion(candidates, target - candidates[i], ans, i+1)
+        ans.pop()
+        while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+            i = i+1
+        self.reccursion(candidates, target, ans, i+1)
+
+        
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.l = []
+        ans = []
+        candidates.sort()
+        self.reccursion(candidates, target, ans, 0)
+        return self.l
