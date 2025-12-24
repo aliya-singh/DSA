a067820cea18539d435ff136c0305c97daab3ab8
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-24 23:17:38 +0530

    Time: 15 ms (81.03%), Space: 12.6 MB (51.71%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reccursion(self, candidates, target, ans, i):
+        if i == len(candidates):
+            if target == 0:
+                self.l.append(ans[:])
+            return
+       
+        
+        if candidates[i] <= target:
+            ans.append(candidates[i])
+            self.reccursion(candidates, target - candidates[i], ans, i)
+            ans.pop()
+        self.reccursion(candidates, target, ans, i+1)
+
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.l = []
+        ans = []
+        self.reccursion(candidates, target, ans, 0)
+        return self.l
+        
