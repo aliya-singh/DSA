5cba8bcd79d93437f75cb33aed5cce2481dfe731
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-24 19:22:14 +0530

    Time: 0 ms (100%), Space: 12.7 MB (46.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,13 +2,12 @@ class Solution(object):
     def reccursion(self, nums, ans, i):
         if i == len(nums):
             self.l.append(ans[:])
-            return 
+            return
         
         ans.append(nums[i])
         self.reccursion(nums, ans, i+1)
         ans.pop()
         self.reccursion(nums, ans, i+1)
-            
 
     def subsets(self, nums):
         """
@@ -17,6 +16,6 @@ class Solution(object):
         """
         self.l = []
         ans = []
-        i = 0
-        self.reccursion(nums, ans, i)
+        self.reccursion(nums, ans, 0)
         return self.l
+        
