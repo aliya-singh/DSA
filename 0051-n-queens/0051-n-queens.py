21bd0a29c3261bd24b545d86ac7b843b0add9a7d
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-29 22:00:44 +0530

    Time: 67 ms (8.73%), Space: 12.6 MB (47.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,58 +1,53 @@
 class Solution(object):
-    def isvalid(self, board, row, col, n):        
+    def isvalid(self, board, row, col, n):
         for i in range(row):
-            if board[i][col] == 'Q':
+            if board[i][col] == "Q":
+                return False
+        
+        for i in range(col):
+            if board[row][i] == "Q":
                 return False
         
         i = row - 1
         j = col - 1
         while i >= 0 and j >= 0:
-            if board[i][j] == 'Q':
+            if board[i][j] == "Q":
                 return False
-            i = i-1
-            j = j-1
+            i -= 1
+            j -= 1
         
         i = row - 1
         j = col + 1
         while i >= 0 and j < n:
-            if board[i][j] == 'Q':
+            if board[i][j] == "Q":
                 return False
             i -= 1
             j += 1
-
+        
         return True
 
-    def reccursion(self, board, n, i):
-        if i == n:
-            solution = []
-            for z in range(len(board)):
+    def reccursion(self, board, n, row):
+        if row == n:
+            sol = []
+            for i in range(len(board)):
                 s = ""
                 for j in range(len(board)):
-                    s += board[z][j]
-                solution.append(s)
-            self.l.append(solution)
-            return 
-                
+                    s += board[i][j]
+                sol.append(s)
+            self.l.append(sol)
         
-        for j in range(n):
-            if self.isvalid(board, i, j, n):
-                board[i][j] = 'Q'
-                self.reccursion(board, n, i+1)
-                board[i][j] = '.'
-
+        for col in range(n):
+            if self.isvalid(board, row, col, n):
+                board[row][col] = "Q"
+                self.reccursion(board, n, row + 1)
+                board[row][col] = "."
 
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
-        s = "." * n
         self.l = []
-        # board = []
-        # for i in range(n):
-        #     board.append(s)
         board = [['.' for _ in range(n)] for _ in range(n)]
-        print(board)
         self.reccursion(board, n, 0)
         return self.l
-        
