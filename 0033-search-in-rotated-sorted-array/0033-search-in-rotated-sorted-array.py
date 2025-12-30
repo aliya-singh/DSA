e9f17bfbd00075ec2e964d53bbe7bc8427d8f5ab
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-31 01:31:26 +0530

    Time: 0 ms (100%), Space: 12.7 MB (43.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,24 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
         start = 0
         end = len(nums) - 1
-
         while start <= end:
-            mid = (start + end)//2
+            mid = (start + end) // 2
+            
             if nums[mid] == target:
                 return mid
-            elif nums[start] <= nums[mid]:
-                if target >= nums[start] and target < nums[mid]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            elif nums[end] >= nums[mid]:
-                if target > nums[mid] and target <= nums[end]:
-                    start = mid + 1
-                else:
-                    end = mid - 1
 
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1  
+            else:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            
         return -1
 
