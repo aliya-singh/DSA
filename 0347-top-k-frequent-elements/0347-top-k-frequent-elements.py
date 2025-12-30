e498f28f0241fd4ccb1266c2856a3796c5236646
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2025-12-31 02:33:25 +0530

    Time: 11 ms (61.12%), Space: 14.2 MB (73.26%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+        
+        return sorted(d, key= lambda x: d[x], reverse=True)[:k]
+    
+
+        
