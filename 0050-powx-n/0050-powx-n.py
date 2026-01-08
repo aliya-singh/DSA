19ce856cc730371ef9ba07f168c0e0a1f0d30f76
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-09 02:46:59 +0530

    Time: 0 ms (100%), Space: 12.4 MB (60.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,21 +1,23 @@
-class Solution(object):
+class Solution(object):        
     def myPow(self, x, n):
         """
         :type x: float
         :type n: int
         :rtype: float
         """
-        
-        ans = 1
-        m = abs(n)
-        while m:
-            if m%2 == 1:
-                ans *= x
-                m = m-1
+        pow = 1
+        neg = False
+        if n < 0:
+            neg = True
+        n = abs(n) 
+        while n > 0:
+            if n % 2 == 1:
+                pow *= x
+                n = n-1
             else:
                 x = x*x
-                m = m/2
+                n = n//2
         
-        if n < 0:
-            ans = 1/ans
-        return ans
+        if neg:
+            return 1/pow
+        return pow
