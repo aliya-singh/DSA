8c20145f32dc6574483f1f3e64b9a1c75eba5c95
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-09 00:28:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,22 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        z = 0
-        o = 0
-        t = 0
+        zero = 0
+        one = 0
+        two = 0
 
-        for i in nums:
-            if i == 0:
-                z += 1
-            elif i == 1:
-                o += 1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zero += 1
+            elif nums[i] == 1:
+                one += 1
             else:
-                t += 1
+                two += 1
         
-        for i in range(z):
-            nums[i] = 0
-        for i in range(o):
-            nums[z+i] = 1
-        for i in range(t):
-            nums[z+i+o] = 2
+        nums[0:zero] = [0] * zero
+        nums[zero:zero+one] = [1] * one
+        nums[zero+one:] = [2] * two
