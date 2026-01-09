5983ca206cfd9fcfda2f07363a974c3591f873f9
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-09 20:08:22 +0530

    Time: 0 ms (100%), Space: 12.7 MB (85.11%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -19,7 +19,7 @@ class Solution(object):
 
             if left_depth > right_depth:
                 return (left_depth + 1, left_node)
-            elif left_depth < right_depth:
+            elif right_depth > left_depth:
                 return (right_depth + 1, right_node)
             else:
                 return (left_depth + 1, node)
