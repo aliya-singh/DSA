14d5973a968887cd688ffbc3ec8fed6c6ecddd19
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-09 23:04:24 +0530

    Time: 0 ms (100%), Space: 12.6 MB (25.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def factorial(self, m):
-        fact = 1
-        for i in range(2, m):
-            fact *= i
-        
-        return fact
-
+class Solution(object):   
     def uniquePaths(self, m, n):
         """
         :type m: int
         :type n: int
         :rtype: int
         """
-
-        a = factorial(m + n - 2)
-        b = factorial(n - 1)
-        c = factorial(m - 1)
-
-        return a / (b * c)
+        def fact(a):
+            f = 1
+            for i in range(2, a+1):
+                f *= i
+            
+            return f
+        
+        a = fact(n+m-2)
+        b = fact(n-1)
+        c = fact(m-1)
+        print(a, b, c)
+        return a / (b*c)
         
