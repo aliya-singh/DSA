1e043eacf39e96dd3cea56dae00f03dd784b1d79
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-10 01:01:50 +0530

    Time: 572 ms (16.73%), Space: 12.4 MB (57.12%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,14 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-
-        nums.sort()
         n = len(nums)
+        nums.sort()
         l = []
         for i in range(n-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
             for j in range(i+1, n-2):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
@@ -20,21 +18,21 @@ class Solution(object):
                 start = j+1
                 end = n-1
                 while start < end:
-                    s = nums[i] + nums[j] + nums[start] + nums[end]
-                    if s > target:
-                        end -= 1
-                    elif s < target:
+                    sum = nums[i] + nums[j] + nums[start] + nums[end]
+                    if sum < target:
                         start += 1
+                    elif sum > target:
+                        end -= 1
                     else:
                         l.append([nums[i], nums[j], nums[start], nums[end]])
                         start += 1
                         end -= 1
+                    
                         while start < end and nums[start] == nums[start-1]:
-                            start = start+1
+                            start += 1    
                         while start < end and nums[end] == nums[end+1]:
-                            end = end - 1
-        
-        return l
-
+                            end -= 1
             
-        
+        return l
+                
+            
