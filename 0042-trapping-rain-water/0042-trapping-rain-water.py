cc0318d6e85246387413650e97430cd812505278
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-11 02:08:53 +0530

    Time: 27 ms (28.09%), Space: 13.8 MB (30.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -6,22 +6,27 @@ class Solution(object):
         """
         n = len(height)
         l1 = [height[0]]
+
         for i in range(1, n):
-            if l1[i-1] < height[i]:
-                l1.append(height[i])
-            else:
+            if l1[i-1] > height[i]:
                 l1.append(l1[i-1])
+            else:
+                l1.append(height[i])
 
         l2 = [0] * n
         l2[n-1] = height[n-1]
         for i in range(n-2, -1, -1):
-            if l2[i+1] > height[i]:
-                l2[i] = l2[i+1]
-            else:
+            if l2[i+1] < height[i]:
                 l2[i] = height[i]
+            else:
+                l2[i] = l2[i+1]
         
         count = 0
         for i in range(n):
             count += min(l1[i], l2[i]) - height[i]
         
         return count
+
+
+
+
