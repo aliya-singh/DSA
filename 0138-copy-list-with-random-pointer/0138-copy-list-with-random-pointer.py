0a880f6fde4e94f34eadf945085a1a4d0dcf2063
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-11 00:49:41 +0530

    Time: 34 ms (76.82%), Space: 13.2 MB (73.11%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -13,24 +13,20 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
-        
-        if head is None:
+        if not head:
             return None
-        
-        temp = head
+
+        itr = head
         d = {}
-        while temp:
-            d[temp] = Node(temp.val)
-            temp = temp.next
+        while itr:
+            d[itr] = Node(itr.val)
+            itr = itr.next
         
-        temp = head
-        while temp:
-            newnode = d[temp]
-            newnode.next = d[temp.next] if temp.next else None
-            newnode.random = d[temp.random] if temp.random else None
-            temp = temp.next
+        itr = head
+        while itr:
+            new_head = d[itr]
+            new_head.next = d[itr.next] if itr.next else None
+            new_head.random = d[itr.random] if itr.random else None
+            itr = itr.next
         
         return d[head]
-            
-        
-        
