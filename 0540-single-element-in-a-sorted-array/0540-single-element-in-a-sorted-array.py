850de3fc09ee445091b5fabacaa0eb4399d6b480
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-12 22:09:16 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
         
         if nums[0] != nums[1]:
@@ -14,12 +15,14 @@ class Solution(object):
             return nums[-1]
         
         start = 1
-        end = len(nums) - 2
-        while start < end:
-            mid = (start + end)//2
-            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
+        end = n - 2
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
                 return nums[mid]
-            elif (mid % 2 == 1 and nums[mid] == nums[mid-1]) or (mid % 2 == 0 and nums[mid] == nums[mid+1]):
-                start += 1
+            elif mid%2 == 0 and nums[mid] == nums[mid-1]:
+                end = mid - 1
+            elif mid%2 == 1 and nums[mid] == nums[mid+1]:
+                end = mid - 1
             else:
-                end -= 1
+                start = mid + 1
