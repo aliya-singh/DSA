47bc60fe5c20a5faafbfafb6e890269659e71be8
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-12 22:13:11 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -10,19 +10,18 @@ class Solution(object):
         
         if nums[0] != nums[1]:
             return nums[0]
-        
-        if nums[-1] != nums[-2]:
-            return nums[-1]
-        
-        start = 1
-        end = n - 2
+
+        if nums[n-1] != nums[n-2]:
+            return nums[n-1]
+            
+        start = 0
+        end = n - 1
         while start <= end:
-            mid = (start + end) // 2
+            mid = (start + end)//2
             if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
                 return nums[mid]
-            elif mid%2 == 0 and nums[mid] == nums[mid-1]:
-                end = mid - 1
-            elif mid%2 == 1 and nums[mid] == nums[mid+1]:
+            elif (mid%2 == 0 and nums[mid] == nums[mid-1]) or (mid%2 == 1 and nums[mid] == nums[mid+1]):
                 end = mid - 1
             else:
                 start = mid + 1
+        
