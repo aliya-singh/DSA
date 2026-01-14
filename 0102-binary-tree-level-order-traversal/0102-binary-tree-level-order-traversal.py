bfb8196629bd51746778d2d04b4d830b79ff97c2
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-15 00:56:53 +0530

    Time: 0 ms (100%), Space: 13.1 MB (79.37%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -30,7 +30,7 @@ class Solution(object):
                 
                 if node.right:
                     queue.append(node.right)
-            level += 1
+            # level += 1
             l.append(ll)
         
         return l
