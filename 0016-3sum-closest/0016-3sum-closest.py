1e22c0bbe3c4af9d10232b8af223ca085c59fd74
Aliya Singh <122780884+aliya-singh@users.noreply.github.com>
2026-01-15 12:20:02 +0530

    Time: 384 ms (94.98%), Space: 12.6 MB (42.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -5,21 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        nums.sort()
         n = len(nums)
-        ans = float("inf")
-        for i in range(n-1):
+        nums.sort()
+        mi = float("inf")
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             start = i + 1
             end = n - 1
             while start < end:
-                sum = nums[start] + nums[end] + nums[i]
-                if abs(target - ans) > abs(target - sum):
+                sum = nums[i] + nums[start] + nums[end]
+                res = abs(sum-target)
+                if res < mi:
                     ans = sum
+                    mi = res
                 if target > sum:
                     start += 1
                 elif target < sum:
                     end -= 1
                 else:
                     return target
-        
+                
+            
         return ans
